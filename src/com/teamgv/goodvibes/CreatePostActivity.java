c0633606bb1a284c2a67ffe699bf5037bd629e35
Julian Wu <cosmobserver@gmail.com>
2013-11-30 00:53:21 -0500

    Post done

--- src/com/teamgv/goodvibes/CreatePostActivity.java
+++ src/com/teamgv/goodvibes/CreatePostActivity.java
@@ -4,21 +4,29 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class CreatePostActivity extends Activity {
 	private static final int CAMERA_REQUEST = 1888; 
     private ImageView imageView;
+    private EditText title;
+    private EditText content;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_post);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
+		this.title = (EditText) this.findViewById(R.id.editText1);
+		this.content = (EditText) this.findViewById(R.id.editText2);
 		this.imageView = (ImageView)this.findViewById(R.id.imageView1);
 	}
 
@@ -28,6 +36,26 @@ public class CreatePostActivity extends Activity {
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.create_post, menu);
 	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.action_post:
+			if (this.title.length() < 1) { 
+				Toast warn = Toast.makeText(this, "Must have a title", Toast.LENGTH_SHORT);
+				warn.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 50);
+				warn.show();
+				return false;
+			}
+			Toast.makeText(this, "You have created a post\nTitle: " 
+								+ this.title.getText() + "\nContent:\n" + this.content.getText(),
+								Toast.LENGTH_LONG).show();
+			
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		
 	}
 	
     public void takePhotoCamera(View v)
@@ -46,5 +74,8 @@ public class CreatePostActivity extends Activity {
             imageView.setImageBitmap(photo);
         }  
     } 
-
+    
+    public void DebugVar(){
+    	Log.d("Post", this.title.getText() + ": " + this.content.getText());
+    }
 }
---
+++ src/com/teamgv/goodvibes/Post.java
@@ -0,0 +1,15 @@
+package com.teamgv.goodvibes;
+
+/**
+ * A post is a post that contains a 
+ *  *Title
+ *  *Content
+ *  *Optional Image
+ *  *Location
+ * @author Julian Wu
+ *
+ */
+public class Post {
+	public String title;
+	
+}
